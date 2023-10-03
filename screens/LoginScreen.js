f3fedba40387beec4daf3aaa0e2a507e6cbe8eca
I-am-boora <sonuboora125@hotmail.com>
2023-10-03 13:22:43 +0530

    login screen design

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -17,6 +17,7 @@ const LoginScreen = () => {
   const navigation = useNavigation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  console.log(email);
   return (
     <View style={styles.container}>
       <Image
