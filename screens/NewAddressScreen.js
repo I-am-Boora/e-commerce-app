dfe86898d0cde08e2b942b0947bdee86b69254ca
I-am-boora <sonuboora125@hotmail.com>
2023-10-13 13:04:56 +0530

    all address show screen added

--- screens/NewAddressScreen.js
+++ screens/NewAddressScreen.js
@@ -1,18 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Pressable } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import CustomHeader from "../components/CustomHeader";
-import { scale } from "react-native-size-matters";
+import { moderateScale, scale, verticalScale } from "react-native-size-matters";
 import { COLOR } from "../Constraints/colors";
+import { Feather } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const NewAddressScreen = () => {
+  const navigation = useNavigation();
   return (
     <SafeAreaView>
       <View style={{ height: scale(50), backgroundColor: COLOR.primary }} />
+      <View style={{ paddingHorizontal: scale(10) }}>
+        <Text
+          style={[
+            styles.text,
+            { fontSize: scale(15), marginTop: verticalScale(10) },
+          ]}
+        >
+          Add Addresses
+        </Text>
+        <Pressable
+          style={styles.container}
+          onPress={() => navigation.navigate("AddAddress")}
+        >
+          <Text style={styles.text}>Add new address</Text>
+          <Feather name="chevron-right" size={24} color="black" />
+        </Pressable>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default NewAddressScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    borderRadius: scale(8),
+    borderWidth: scale(1),
+    borderColor: "grey",
+    padding: moderateScale(10),
+    marginVertical: verticalScale(10),
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: scale(13),
+    fontWeight: "500",
+  },
+});
